ced9ee9e6f676d75a69a0d14db157a1664ab0134
Deineee <deinepomperada@gmail.com>
2025-07-26 00:43:27 +0800

    reverted some things

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
 
   after_initialize :set_default_role, if: :new_record?
